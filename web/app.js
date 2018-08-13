1b5edee5e95a789d68b3b3aef252e59c94d34281
cubap <cubap@slu.edu>
2018-08-13 11:15:27 -0500

    just formatting

--- web/app.js
+++ web/app.js
@@ -1,7 +1,7 @@
-async function get (url) {
+async function get(url) {
 
     // shortcut
-    if(url.length<6){
+    if (url.length < 6) {
         return JSON.parse(localStorage.getItem(url))
     }
 
@@ -28,105 +28,109 @@ async function get (url) {
     xhr.send();
 }
 
-function expand(obj){
+function expand(obj) {
     let toRender = {}
     let findId = obj["@id"]
     let annos = findById(findId)
     // TODO: attach evidence to each property value
     // add each value in a predictable way
     // type properties for possible rendering?
-    for(let i=0;i<annos.length;i++){
-        for(let j=0;j<annos[i].body.length;j++){
-            if(annos[i].body[j].evidence) {
-                let evId = (typeof annos[i].body[j].evidence==="object") ? annos[i].body[j].evidence["@id"] : annos[i].body[j].evidence
+    for (let i = 0; i < annos.length; i++) {
+        for (let j = 0; j < annos[i].body.length; j++) {
+            if (annos[i].body[j].evidence) {
+                let evId = (typeof annos[i].body[j].evidence === "object") ? annos[i].body[j].evidence["@id"] : annos[i].body[j].evidence
                 obj.evidence = JSON.parse(localStorage.getItem(evId))
             } else {
-                obj = Object.assign(annos[i].body[j],obj)
+                obj = Object.assign(annos[i].body[j], obj)
             }
         }
     }
     return obj
 }
 
-function findById(id){
-    let everything = Object.keys(localStorage).map(k=>(k&&k.length===4)&&JSON.parse(localStorage.getItem(k)))
-    let matches = everything.filter(o=>o.target===id)
+function findById(id) {
+    let everything = Object.keys(localStorage).map(k => (k && k.length === 4) && JSON.parse(localStorage.getItem(k)))
+    let matches = everything.filter(o => o.target === id)
     return matches
 }
 
-function renderEvidence(obj){
+function renderEvidence(obj) {
     let evidence = document.createElement("a")
-    let elink = (typeof obj.evidence==="object") ? obj.evidence["@id"] : obj.evidence
-    evidence.setAttribute("href",elink)
-    evidence.setAttribute("target","_blank")
+    let elink = (typeof obj.evidence === "object") ? obj.evidence["@id"] : obj.evidence
+    evidence.setAttribute("href", elink)
+    evidence.setAttribute("target", "_blank")
     evidence.className = "mc-evidence"
     evidence.textContent = obj.evidence.label || "View evidence"
     return evidence
 }
 
 function renderPerson(obj, elem) {
-elem.innerHTML = "<h3>" + (obj.label || "unlabeled") + "</h3>"
-elem.append(renderName(obj))
-elem.append(renderGender(obj))
-elem.append(renderProp(obj,"birthDate"))
-elem.append(renderProp(obj,"deathDate"))
-elem.append(renderEvidence(obj))
-document.getElementById("obj-viewer").append(renderObject(obj))
+    elem.innerHTML = "<h3>" + (obj.label || "unlabeled") + "</h3>"
+    elem.append(renderName(obj))
+    elem.append(renderGender(obj))
+    elem.append(renderProp(obj, "birthDate"))
+    elem.append(renderProp(obj, "deathDate"))
+    elem.append(renderEvidence(obj))
+    document.getElementById("obj-viewer").append(renderObject(obj))
 }
+
 function renderName(obj) {
-let elem;
-let joined = obj.familyName + ", " + obj.givenName;
-if (joined) {
-    elem = document.createElement("span")
-    elem.textContent = joined
-    elem.className = "mc-name"
+    let elem;
+    let joined = obj.familyName + ", " + obj.givenName;
+    if (joined) {
+        elem = document.createElement("span")
+        elem.textContent = joined
+        elem.className = "mc-name"
+    }
+    return elem || null
 }
-return elem || null
+
+function renderProp(obj, prop) {
+    let elem;
+    if (obj[prop]) {
+        elem = document.createElement("div")
+        elem.textContent = prop + ": " + obj[prop]
+        elem.className = "mc-" + prop
+    }
+    return elem || null
 }
-function renderProp(obj,prop) {
-let elem;
-if(obj[prop]){
-    elem = document.createElement("div")
-    elem.textContent = prop + ": " + obj[prop]
-    elem.className = "mc-"+prop
-}
-return elem || null
-}
+
 function renderGender(obj) {
-let elem;
-if (obj.gender) {
-    elem = document.createElement("span")
-    elem.innerHTML = (obj.gender === "male") ? "&male;" : "&female;"
-    elem.className = "mc-gender"
-}	            
-return elem || null
+    let elem;
+    if (obj.gender) {
+        elem = document.createElement("span")
+        elem.innerHTML = (obj.gender === "male") ? "&male;" : "&female;"
+        elem.className = "mc-gender"
+    }
+    return elem || null
 }
+
 function renderObject(obj) {
-return document.createElement("span").textContent = JSON.stringify(obj, null, 4)
+    return document.createElement("span").textContent = JSON.stringify(obj, null, 4)
 }
 
-function renderLocation(){
-let elem = document.getElementById("mc-location")
-let cemetery = expand(JSON.parse(localStorage.getItem("l001")))
-elem.innerHTML = "<h2>"+cemetery.label+"</h2>"
-let ref = document.createElement("a")
-ref.setAttribute("href",cemetery.seeAlso)
-ref.setAttribute("target","_blank")
-ref.className = "seeAlso"
-ref.textContent = cemetery.seeAlso
-elem.append(ref) 
+function renderLocation() {
+    let elem = document.getElementById("mc-location")
+    let cemetery = expand(JSON.parse(localStorage.getItem("l001")))
+    elem.innerHTML = "<h2>" + cemetery.label + "</h2>"
+    let ref = document.createElement("a")
+    ref.setAttribute("href", cemetery.seeAlso)
+    ref.setAttribute("target", "_blank")
+    ref.className = "seeAlso"
+    ref.textContent = cemetery.seeAlso
+    elem.append(ref)
 }
 
-function loadLists(locationId){
-// find references to location
-// parse list for people
+function loadLists(locationId) {
+    // find references to location
+    // parse list for people
 }
 
-function renderResidents(list){
-// create links for each person in the location
-// render on click
+function renderResidents(list) {
+    // create links for each person in the location
+    // render on click
 }
 
 // load defaulty bits
 renderLocation()
-renderPerson(expand(JSON.parse(localStorage.getItem("p001"))),document.getElementById("view"))
+renderPerson(expand(JSON.parse(localStorage.getItem("p001"))), document.getElementById("view"))
