a01a6885ec191704c6bde76290b2ed34186f070d
cubap <cubap@slu.edu>
2018-08-15 23:44:08 -0500

    edit form for person
    updates the obj in place and rerenders
    many limits right now
    TODO:saving, flagging dirty

--- web/app.js
+++ web/app.js
@@ -1,7 +1,7 @@
 var mc = {}
 mc.focusObject = document.getElementById("mc-view")
 
-mc.focusOn = function (id) {
+mc.focusOn = function(id) {
     mc.focusObject.setAttribute('mc-object', id)
 }
 
@@ -14,7 +14,7 @@ function get(url) {
 
     let xhr = new XMLHttpRequest();
     xhr.open("GET", url);
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
             let obj, err;
             if (xhr.status === 200) {
@@ -39,9 +39,9 @@ function expand(obj) {
     let toRender = {}
     let findId = obj["@id"]
     let annos = findById(findId)
-    // TODO: attach evidence to each property value
-    // add each value in a predictable way
-    // type properties for possible rendering?
+        // TODO: attach evidence to each property value
+        // add each value in a predictable way
+        // type properties for possible rendering?
     for (let i = 0; i < annos.length; i++) {
         for (let j = 0; j < annos[i].body.length; j++) {
             if (annos[i].body[j].evidence) {
@@ -63,50 +63,43 @@ function findById(id) {
 
 var template = {}
 
-template.evidence = function (obj) {
-    if (!obj.evidence) {return null}
+template.evidence = function(obj) {
+    if (!obj.evidence) { return null }
     return `<a class="mc-evidence" href="${(typeof obj.evidence === "object") ? obj.evidence["@id"] : obj.evidence}" target="_blank">${obj.evidence.label || "View evidence"}</a>`
 }
 
-template.fullName = function (obj) {
-    if (!obj || !(obj.givenName || obj.familyName)) {return null}
+template.fullName = function(obj) {
+    if (!obj || !(obj.givenName || obj.familyName)) { return null }
     return `<span class="mc-name">${obj.familyName||"[ unknown ]"}, ${obj.givenName}</span>`
 }
 
-template.prop = function (obj, prop) {
-    if(!obj.hasOwnProperty(prop)) {return ``}
+template.prop = function(obj, prop) {
+    if (!obj.hasOwnProperty(prop)) { return `` }
     return `<span class="mc-${prop.trim().replace(/\s+/g,"-").normalize("NFC").toLowerCase()}">${prop}: ${obj[prop] || "[ undefined ]"}</span>`
 }
 
-template.gender = function (obj) {
+template.gender = function(obj) {
     return `<span class="mc-gender">${(obj.gender === "male") ? "&male;" : "&female;"}</span>`
 }
 
-template.JSON = function (obj) {
-    try{
+template.JSON = function(obj) {
+    try {
         return `${JSON.stringify(obj, null, 4)}`
-    }
-    catch (err){
+    } catch (err) {
         return null
     }
 }
 
-template.location = function () {
+template.location = function() {
     let cemetery = expand(JSON.parse(localStorage.getItem("l001")))
-    if(!cemetery) {return null}
+    if (!cemetery) { return null }
     return `<h2>${cemetery.label}</h2>
     <a href="${cemetery.seeAlso}" target="_blank" class="mc-see-also">${cemetery.seeAlso}</a>`
 }
 
-template.personForm = function (person) {
-    return `<form>
-    <label for="mc-label">Label: <input id="mc-label" type="text" placeholder="simple name" value="${ person.label }" ></label>
-    </form>`
-}
-
-template.list = function (obj, elem) {
+template.list = function(obj, elem) {
     if (typeof obj.resources === "string") {
-        get(obj.resources).then(function (ls) {
+        get(obj.resources).then(function(ls) {
             obj.resources = ls
             return template.list(obj, elem)
         })
@@ -118,8 +111,8 @@ template.list = function (obj, elem) {
     return ul += `</ul>`
 }
 
-template.byObjectType = function (obj) {
-    let templateFunction = function () {}
+template.byObjectType = function(obj) {
+    let templateFunction = function() {}
     switch (obj["@type"]) {
         case "Person":
             templateFunction = template.person
@@ -140,7 +133,7 @@ template.byObjectType = function (obj) {
     return templateFunction(obj)
 }
 
-template.person = function (obj) {
+template.person = function(obj, hideEditForm) {
     let elem = `<h3>${obj.label || "unlabeled"}</h3>`
     let tmp = [
         template.fullName(obj),
@@ -150,8 +143,47 @@ template.person = function (obj) {
         template.evidence(obj)
     ]
     elem += tmp.join("\n")
-    document.getElementById("mc-person-form").innerHTML = template.personForm(obj)
+    if (!hideEditForm) {
+        let pForm = document.getElementById("mc-person-form")
+        pForm.innerHTML = template.personForm(obj)
+        for (el of pForm.getElementsByTagName("input")) {
+            el.onchange = function(event) {
+                let prop = event.target.getAttribute("id").substr(3).replace(/(\-\w)/g, function(m) { return m[1].toUpperCase(); })
+                obj[prop] = event.target.value
+                renderElement(mc.focusObject, template.person(obj, true))
+                renderElement(document.getElementById("obj-viewer"), template.JSON(obj))
+            }
+            el.addEventListener('input', el.onchange)
+        }
+    }
     return elem
+}
+
+template.personForm = function(person) {
+    return `<form class="mc-person-edit">
+    <input type="hidden" value="Person" id="type" >
+    <label for="mc-label">Full Name: 
+        <input id="mc-label" type="text" placeholder="full name" value="${ person.label || person.name || "" }" >
+    </label>
+    <label for="mc-birth-date">Birth Date: 
+        <input id="mc-birth-date" type="date" placeholder="YYYY-MM-DD" value="${ person.birthDate || "" }" >
+    </label>
+    <label for="mc-death-date">Death Date: 
+        <input id="mc-death-date" type="date" placeholder="simple name" value="${ person.deathDate || "" }" >
+    </label>
+    <label for="mc-given-name">Given Name: 
+        <input id="mc-given-name" type="text" placeholder="first name" value="${ person.givenName || "" }" >
+    </label>
+    <label for="mc-family-name">Family Name: 
+        <input id="mc-family-name" type="text" placeholder="last name" value="${ person.familyName || "" }" >
+    </label>
+    <label for="mc-maiden-name">Maiden Name: 
+        <input id="mc-maiden-name" type="text" placeholder="former name" value="${ person.maidenName || "" }" >
+    </label>
+    <label for="mc-evidence">Evidence: 
+        <input id="mc-evidence" type="url" placeholder="just urls for now" value="${ (person.evidence&&person.evidence['@id']) || person.evidence || "" }" >
+    </label>
+    </form>`
 }
 
 function renderElement(elem, tmp) {
@@ -192,11 +224,11 @@ mc.focusObject.setAttribute("mc-object", "li01")
 const CREATE_URL = "http://tiny.rerum.io/app/create"
 
 function createPerson() {
-    const callback = function (error, result) {
+    const callback = function(error, result) {
         // create, rewrite id
     }
     let form = document.getElementById("mc-person-form")
-    // TODO: Make this make sense
+        // TODO: Make this make sense
     let obj = {
         "@type": "Person",
         "label": form.getElementById("mc-label"),
@@ -212,7 +244,7 @@ function post(callback, url, obj) {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, false);
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
             let res, err;
             if (xhr.status === 201) {
