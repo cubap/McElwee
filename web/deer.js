63a931bd4e54fd3f1d33bd19aa3fcd4acaed787c
cubap <cubap@slu.edu>
2018-09-27 17:55:31 -0500

    deer bits for annotation and creation.

--- web/deer.js
+++ web/deer.js
@@ -8,56 +8,177 @@
  */
 class Deer {
     constructor(collectionURL) {
-        this.collectionURL = collectionURL
-        this.collection = localStorage.getItem(collectionURL) || {
-            "@context": "",
-            "@type": "List",
-            "label": "List of Entities",
-            "resources": [],
-            "__rerum": {
-                "alpha": "true",
-                "APIversion": "0.8.0",
-                "createdAt": Date.now(),
-                "isOverwritten": "",
-                "isReleased": "",
-                "history": {
-                    "next": [],
-                    "previous": "",
-                    "prime": "root"
+            this.collectionURL = collectionURL
+            this.collection = localStorage.getItem(collectionURL) || {
+                "@context": "",
+                "@type": "List",
+                "label": "List of Entities",
+                "resources": [],
+                "__rerum": {
+                    "alpha": "true",
+                    "APIversion": "0.8.0",
+                    "createdAt": Date.now(),
+                    "isOverwritten": "",
+                    "isReleased": "",
+                    "history": {
+                        "next": [],
+                        "previous": "",
+                        "prime": "root"
+                    },
+                    "releases": {
+                        "next": [],
+                        "previous": "",
+                        "replaces": ""
+                    },
+                    "generatedBy": "DEER tool"
                 },
-                "releases": {
-                    "next": [],
-                    "previous": "",
-                    "replaces": ""
-                },
-                "generatedBy": "DEER tool"
-            },
-            "@id": collectionURL        
+                "@id": collectionURL
+            }
+            this.resources = []
+            if (!localStorage.getItem("CURRENT_LIST_ID")) {
+                localStorage.setItem("CURRENT_LIST_ID", collectionURL)
+            }
+            this.default = {
+                context: "https:schema.org",
+                type: "Thing",
+                name: "new Entity",
+                creator: "https://undefined.net"
+            }
+
+            // "Constants"
+            this.DEFAULT_LIST_ID = "li01"
+            this.BASE_ID = "http://devstore.rerum.io/v1"
+            this.CREATE_URL = "http://tinydev.rerum.io/app/create"
+            this.UPDATE_URL = "http://tinydev.rerum.io/app/update"
         }
-        this.resources = []
-        if(!localStorage.getItem("CURRENT_LIST_ID")) {
-            localStorage.setItem("CURRENT_LIST_ID",collectionURL)
-        }
-        this.default = {
-            context: "https:schema.org",
-            type: "Thing",
-            name: "new Entity"
-        }
-    }
-    create(obj) {
+        /**
+         * Generate a new object URI for a resource. Abstract additional
+         * properties to annotations.
+         * @param {Object} obj complete resource to process
+         * @param {Object} attribution creator and generator identities
+         */
+    async create(obj, attribution, evidence) {
         let mint = {
-            "@context": this.default.context,
-            "@type": this.default.type,
-            "name":this.default.name
+            "@context": obj["@context"] || this.default.context,
+            "@type": obj["@type"] || this.default.type,
+            "name": this.getValue(obj.name || obj.label) || this.default.name,
+            "creator": attribution || this.default.creator
         }
+        if (evidence) {
+            mint.evidence = evidence
+        }
+        const newObj = await fetch(CREATE_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify(mint)
+        })
+        .then(response => response.json())
+        const listID = localStorage.getItem("CURRENT_LIST_ID") || this.DEFAULT_LIST_ID
+        let list = await get(listID)
+        const objID = newObj.new_obj_state["@id"]
+        list.resources.push({
+            "@id": objID,
+            "label": newObj.new_obj_state.name
+        })
+        try {
+            list = await fetch(UPDATE_URL, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                },
+                body: JSON.stringify(list)
+            })
+            .then(response => response.json().new_obj_state)
+            .catch(err => Promise.reject(err))
+        } catch (err) {}
+        localStorage.setItem(list["@id"], JSON.stringify(list))
+        localStorage.setItem("CURRENT_LIST_ID", list["@id"])
+        let annotations = []
+        for (var key of Object.keys(obj)) {
+            if (["@context", "@type", "name"].indexOf(key) > -1) {
+                continue
+            }
+            let annotation = {
+                "@context": "",
+                "@type": "Annotation",
+                "motivation": "describing",
+                "target": objID,
+                "body": {}
+            }
+            annotation.body[key] = obj[key]          
+            if (attribution) {
+                annotation.creator = attribution
+            }
+            if (evidence) {
+                annotation.evidence = evidence
+            }
+            annotations.push(annotation)
+        }
+        let temp = await Promise.all(annotations.map(annotate))
+        return newObj.new_obj_state
     }
-    getValue(property) {
-        // TODO: There must be a best way to do this...
-        if (!Array.isArray(property)) {
-            property = [property]
+    async annotate(annotation) {
+        let config = {
+            url: annotation["@id"] ? this.UPDATE_URL : this.CREATE_URL,
+            method: annotation["@id"] ? "PUT" : "POST",
+            body: annotation
         }
-        for(p of property) {
-            //get value, organize complicatedly.
+        return await fetch(config.url, {
+            method: config.method,
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify(config.body)
+        }).catch(error => console.error('Error:', error))
+        .then(response => response.json())
+        .then(function (newState) {
+            localStorage.setItem(newState["@id"], JSON.stringify(newState.new_obj_state))
+            return newState.new_obj_state
+        })
+    }
+    getValue(property, asType) {
+        // TODO: There must be a best way to do this...
+        let prop;
+        if (!Array.isArray(property)) {
+            prop = property.map(getValue)
+        }
+        if (typeof property === "object") {
+            // TODO: JSON-LD insists on "@value", but this is simplified in a lot
+            // of contexts. Reading that is ideal in the future.
+            prop =
+                p.hasOwnProperty("@value") && p["@value"] ||
+                p.hasOwnProperty("value") && p["value"] ||
+                p.hasOwnProperty("$value") && p["$value"] ||
+                p.hasOwnProperty("val") && p["val"]
+        } else {
+            prop = property
+        }
+        try {
+            switch (asType.toUpperCase()) {
+                case "STRING":
+                    prop = prop.toString();
+                    break
+                case "NUMBER":
+                    prop = parseFloat(prop);
+                    break
+                case "INTEGER":
+                    prop = parseInt(prop);
+                    break
+                case "BOOLEAN":
+                    prop = !Boolean(["false", "no", "0", "", "undefined", "null"].indexOf(String(prop).toLowerCase().trim()));
+                    break
+                default:
+            }
+        } catch (err) {
+            if (asType) {
+                throw new Error("asType: '" + asType + "' is not possible.\n" + err.message)
+            } else {
+                // no casting requested
+            }
+        } finally {
+            return prop
         }
     }
 }
