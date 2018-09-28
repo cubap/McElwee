437bd91f1e5439f6649bb7c7ba1f56cc0faacee1
cubap <cubap@slu.edu>
2018-09-28 14:23:20 -0500

    updating class methods for rendering

--- web/deer.js
+++ web/deer.js
@@ -38,18 +38,27 @@ class Deer {
             if (!localStorage.getItem("CURRENT_LIST_ID")) {
                 localStorage.setItem("CURRENT_LIST_ID", collectionURL)
             }
+            
             this.default = {
                 context: "https:schema.org",
                 type: "Thing",
                 name: "new Entity",
                 creator: "https://undefined.net"
             }
-
+            
             // "Constants"
             this.DEFAULT_LIST_ID = "li01"
             this.BASE_ID = "http://devstore.rerum.io/v1"
             this.CREATE_URL = "http://tinydev.rerum.io/app/create"
             this.UPDATE_URL = "http://tinydev.rerum.io/app/update"
+            this.TYPES = ["Event", "Person", "Location", "List", "Thing"]
+            this.FOCUS_OBJECT = document.getElementsByTagName("deer-view")[0] || document.getElementById("deer-view")
+
+            this.renderObserver = new MutationObserver(observerCallback)
+            this.renderObserver.observe(this.FOCUS_OBJECT, {
+                attributes: true
+            })
+
         }
         /**
          * Generate a new object URI for a resource. Abstract additional
@@ -68,13 +77,13 @@ class Deer {
             mint.evidence = evidence
         }
         const newObj = await fetch(CREATE_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json; charset=utf-8"
-            },
-            body: JSON.stringify(mint)
-        })
-        .then(response => response.json())
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                },
+                body: JSON.stringify(mint)
+            })
+            .then(response => response.json())
         const listID = localStorage.getItem("CURRENT_LIST_ID") || this.DEFAULT_LIST_ID
         let list = await get(listID)
         const objID = newObj.new_obj_state["@id"]
@@ -84,14 +93,14 @@ class Deer {
         })
         try {
             list = await fetch(UPDATE_URL, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json; charset=utf-8"
-                },
-                body: JSON.stringify(list)
-            })
-            .then(response => response.json().new_obj_state)
-            .catch(err => Promise.reject(err))
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json; charset=utf-8"
+                    },
+                    body: JSON.stringify(list)
+                })
+                .then(response => response.json().new_obj_state)
+                .catch(err => Promise.reject(err))
         } catch (err) {}
         localStorage.setItem(list["@id"], JSON.stringify(list))
         localStorage.setItem("CURRENT_LIST_ID", list["@id"])
@@ -107,7 +116,7 @@ class Deer {
                 "target": objID,
                 "body": {}
             }
-            annotation.body[key] = obj[key]          
+            annotation.body[key] = obj[key]
             if (attribution) {
                 annotation.creator = attribution
             }
@@ -116,71 +125,125 @@ class Deer {
             }
             annotations.push(annotation)
         }
-        let temp = await Promise.all(annotations.map(annotate))
+        // just enforcing the delay
+        let temp = await Promise.all(annotations.map(upsert))
         return newObj.new_obj_state
     }
-    async annotate(annotation) {
+
+    /**
+     * Update or create object in database. 
+     * @param {Object} obj object to write to database
+     * @returns {Promise} Fetch write to database resolves in new object state
+     */
+    upsert(obj) {
+        // TODO: stub header or _id property to force the object ID in MongoDB
         let config = {
-            url: annotation["@id"] ? this.UPDATE_URL : this.CREATE_URL,
-            method: annotation["@id"] ? "PUT" : "POST",
-            body: annotation
+            url: obj["@id"] ? this.UPDATE_URL : this.CREATE_URL,
+            method: obj["@id"] ? "PUT" : "POST",
+            body: obj
         }
-        return await fetch(config.url, {
-            method: config.method,
-            headers: {
-                "Content-Type": "application/json; charset=utf-8"
-            },
-            body: JSON.stringify(config.body)
-        }).catch(error => console.error('Error:', error))
-        .then(response => response.json())
-        .then(function (newState) {
-            localStorage.setItem(newState["@id"], JSON.stringify(newState.new_obj_state))
-            return newState.new_obj_state
-        })
+        return fetch(config.url, {
+                method: config.method,
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                },
+                body: JSON.stringify(config.body)
+            }).catch(error => console.error('Error:', error))
+            .then(response => response.json())
+            .then(function(newState) {
+                localStorage.setItem(newState["@id"], JSON.stringify(newState.new_obj_state))
+                return newState.new_obj_state
+            })
     }
     getValue(property, asType) {
-        // TODO: There must be a best way to do this...
-        let prop;
-        if (!Array.isArray(property)) {
-            prop = property.map(getValue)
-        }
-        if (typeof property === "object") {
-            // TODO: JSON-LD insists on "@value", but this is simplified in a lot
-            // of contexts. Reading that is ideal in the future.
-            prop =
-                p.hasOwnProperty("@value") && p["@value"] ||
-                p.hasOwnProperty("value") && p["value"] ||
-                p.hasOwnProperty("$value") && p["$value"] ||
-                p.hasOwnProperty("val") && p["val"]
-        } else {
-            prop = property
-        }
-        try {
-            switch (asType.toUpperCase()) {
-                case "STRING":
-                    prop = prop.toString();
-                    break
-                case "NUMBER":
-                    prop = parseFloat(prop);
-                    break
-                case "INTEGER":
-                    prop = parseInt(prop);
-                    break
-                case "BOOLEAN":
-                    prop = !Boolean(["false", "no", "0", "", "undefined", "null"].indexOf(String(prop).toLowerCase().trim()));
-                    break
-                default:
+            // TODO: There must be a best way to do this...
+            let prop;
+            if (!Array.isArray(property)) {
+                prop = property.map(getValue)
             }
-        } catch (err) {
-            if (asType) {
-                throw new Error("asType: '" + asType + "' is not possible.\n" + err.message)
+            if (typeof property === "object") {
+                // TODO: JSON-LD insists on "@value", but this is simplified in a lot
+                // of contexts. Reading that is ideal in the future.
+                prop =
+                    p.hasOwnProperty("@value") && p["@value"] ||
+                    p.hasOwnProperty("value") && p["value"] ||
+                    p.hasOwnProperty("$value") && p["$value"] ||
+                    p.hasOwnProperty("val") && p["val"]
             } else {
-                // no casting requested
+                prop = property
             }
-        } finally {
-            return prop
+            try {
+                switch (asType.toUpperCase()) {
+                    case "STRING":
+                        prop = prop.toString();
+                        break
+                    case "NUMBER":
+                        prop = parseFloat(prop);
+                        break
+                    case "INTEGER":
+                        prop = parseInt(prop);
+                        break
+                    case "BOOLEAN":
+                        prop = !Boolean(["false", "no", "0", "", "undefined", "null"].indexOf(String(prop).toLowerCase().trim()));
+                        break
+                    default:
+                }
+            } catch (err) {
+                if (asType) {
+                    throw new Error("asType: '" + asType + "' is not possible.\n" + err.message)
+                } else {
+                    // no casting requested
+                }
+            } finally {
+                return prop
+            }
+        }
+        /**
+         * Update the DOM with a template from the application.
+         * @param {HTMLElement} elem The DOM Element in which the template will be placed
+         * @param {function} tmp Function to return Template literal
+         */
+    async renderElement(elem, tmp) {
+        while (elem.firstChild) {
+            elem.removeChild(elem.firstChild)
+        }
+        if (tmp) {
+            elem.innerHTML = await tmp
         }
     }
+
+    /**
+     * Removes known "@type" names and sets the one passed in.
+     * Intended for Elements representing an entity for styling.
+     * @param {String} className to be set
+     */
+    setClass(className) {
+        // TODO: Config a list of these and run remove(...CLASSES) instead
+        this.FOCUS_OBJECT.classList.remove(...this.TYPES)
+        this.FOCUS_OBJECT.classList.add(className)
+    }
+
+    async observerCallback(mutationsList) {
+        for (var mutation of mutationsList) {
+            if (mutation.attributeName === "deer-object") {
+                let id = this.FOCUS_OBJECT.getAttribute("deer-object")
+                let data = await expand(await get(id))
+                renderElement(this.FOCUS_OBJECT, template.byObjectType(data))
+            }
+        }
+    }
+
+    /**
+     * Triggers a render call with a new primary item in focus.
+     * This may also be useful as a # or path.
+     * @param {String} id URL or URI that identifies the object
+     */
+    focusOn(id) {
+        this.FOCUS_OBJECT.setAttribute('deer-object', id)
+    }
+
+
+
 }
 
 export default Deer
