53a3d34841faee9aab51b9f332853b961d989b13
cubap <cubap@slu.edu>
2018-10-02 11:37:15 -0500

    entity classing?

--- web/deer.js
+++ web/deer.js
@@ -8,73 +8,73 @@
  */
 class Deer {
     constructor(collectionURL) {
-            this.collectionURL = collectionURL
-            this.collection = localStorage.getItem(collectionURL) || {
-                "@context": "",
-                "@type": "List",
-                "label": "List of Entities",
-                "resources": [],
-                "__rerum": {
-                    "alpha": "true",
-                    "APIversion": "0.8.0",
-                    "createdAt": Date.now(),
-                    "isOverwritten": "",
-                    "isReleased": "",
-                    "history": {
-                        "next": [],
-                        "previous": "",
-                        "prime": "root"
-                    },
-                    "releases": {
-                        "next": [],
-                        "previous": "",
-                        "replaces": ""
-                    },
-                    "generatedBy": "DEER tool"
+        this.collectionURL = collectionURL
+        this.collection = localStorage.getItem(collectionURL) || {
+            "@context": "",
+            "@type": "List",
+            "label": "List of Entities",
+            "resources": [],
+            "__rerum": {
+                "alpha": "true",
+                "APIversion": "0.8.0",
+                "createdAt": Date.now(),
+                "isOverwritten": "",
+                "isReleased": "",
+                "history": {
+                    "next": [],
+                    "previous": "",
+                    "prime": "root"
                 },
-                "@id": collectionURL
-            }
-            this.resources = []
-            if (!localStorage.getItem("CURRENT_LIST_ID")) {
-                localStorage.setItem("CURRENT_LIST_ID", collectionURL)
-            }
-
-            this.default = {
-                context: "https:schema.org",
-                type: "Thing",
-                name: "new Entity",
-                creator: "https://undefined.net"
-            }
-
-            // "Constants"
-            this.DEFAULT_LIST_ID = "li01"
-            this.BASE_ID = "http://devstore.rerum.io/v1"
-            this.CREATE_URL = "http://tinydev.rerum.io/app/create"
-            this.UPDATE_URL = "http://tinydev.rerum.io/app/update"
-            this.QUERY_URL = "http://tinydev.rerum.io/app/query"
-            this.TYPES = ["Event", "Person", "Location", "List", "Thing"]
-            this.FOCUS_OBJECT = document.getElementsByTagName("deer-view")[0] || document.getElementById("deer-view")
-
-            this.TEMPLATES = {
-                Person: this.renderPerson,
-                List: this.renderList,
-                Event: this.renderEvent,
-                default: this.renderEntity,
-                json: this.renderJSON
-            }
-
-            this.newObjectLoader = new MutationObserver(newObjectRender)
-            this.newObjectLoader.observe(this.FOCUS_OBJECT, {
-                attributes: true
-            })
-
+                "releases": {
+                    "next": [],
+                    "previous": "",
+                    "replaces": ""
+                },
+                "generatedBy": "DEER tool"
+            },
+            "@id": collectionURL
         }
-        /**
-         * Generate a new object URI for a resource. Abstract additional
-         * properties to annotations.
-         * @param {Object} obj complete resource to process
-         * @param {Object} attribution creator and generator identities
-         */
+        this.resources = []
+        if (!localStorage.getItem("CURRENT_LIST_ID")) {
+            localStorage.setItem("CURRENT_LIST_ID", collectionURL)
+        }
+
+        this.default = {
+            context: "https:schema.org",
+            type: "Thing",
+            name: "new Entity",
+            creator: "https://undefined.net"
+        }
+
+        // "Constants"
+        this.DEFAULT_LIST_ID = "li01"
+        this.BASE_ID = "http://devstore.rerum.io/v1"
+        this.CREATE_URL = "http://tinydev.rerum.io/app/create"
+        this.UPDATE_URL = "http://tinydev.rerum.io/app/update"
+        this.QUERY_URL = "http://tinydev.rerum.io/app/query"
+        this.TYPES = ["Event", "Person", "Location", "List", "Thing"]
+        this.FOCUS_OBJECT = document.getElementsByTagName("deer-view")[0] || document.getElementById("deer-view")
+
+        this.TEMPLATES = {
+            Person: this.renderPerson,
+            List: this.renderList,
+            Event: this.renderEvent,
+            default: this.renderEntity,
+            json: this.renderJSON
+        }
+
+        this.newObjectLoader = new MutationObserver(newObjectRender)
+        this.newObjectLoader.observe(this.FOCUS_OBJECT, {
+            attributes: true
+        })
+
+    }
+    /**
+     * Generate a new object URI for a resource. Abstract additional
+     * properties to annotations.
+     * @param {Object} obj complete resource to process
+     * @param {Object} attribution creator and generator identities
+     */
     async create(obj, attribution, evidence) {
         let mint = {
             "@context": obj["@context"] || this.default.context,
@@ -159,59 +159,59 @@ class Deer {
                 body: JSON.stringify(config.body)
             }).catch(error => console.error('Error:', error))
             .then(response => response.json())
-            .then(function(newState) {
+            .then(function (newState) {
                 localStorage.setItem(newState["@id"], JSON.stringify(newState.new_obj_state))
                 return newState.new_obj_state
             })
     }
     getValue(property, asType) {
-            // TODO: There must be a best way to do this...
-            let prop;
-            if (!Array.isArray(property)) {
-                prop = property.map(getValue)
-            }
-            if (typeof property === "object") {
-                // TODO: JSON-LD insists on "@value", but this is simplified in a lot
-                // of contexts. Reading that is ideal in the future.
-                prop =
-                    p.hasOwnProperty("@value") && p["@value"] ||
-                    p.hasOwnProperty("value") && p["value"] ||
-                    p.hasOwnProperty("$value") && p["$value"] ||
-                    p.hasOwnProperty("val") && p["val"]
-            } else {
-                prop = property
-            }
-            try {
-                switch (asType.toUpperCase()) {
-                    case "STRING":
-                        prop = prop.toString();
-                        break
-                    case "NUMBER":
-                        prop = parseFloat(prop);
-                        break
-                    case "INTEGER":
-                        prop = parseInt(prop);
-                        break
-                    case "BOOLEAN":
-                        prop = !Boolean(["false", "no", "0", "", "undefined", "null"].indexOf(String(prop).toLowerCase().trim()));
-                        break
-                    default:
-                }
-            } catch (err) {
-                if (asType) {
-                    throw new Error("asType: '" + asType + "' is not possible.\n" + err.message)
-                } else {
-                    // no casting requested
-                }
-            } finally {
-                return prop
-            }
+        // TODO: There must be a best way to do this...
+        let prop;
+        if (!Array.isArray(property)) {
+            prop = property.map(getValue)
         }
-        /**
-         * Update the DOM with a template from the application.
-         * @param {HTMLElement} elem The DOM Element in which the template will be placed
-         * @param {function} tmp Function to return Template literal
-         */
+        if (typeof property === "object") {
+            // TODO: JSON-LD insists on "@value", but this is simplified in a lot
+            // of contexts. Reading that is ideal in the future.
+            prop =
+                p.hasOwnProperty("@value") && p["@value"] ||
+                p.hasOwnProperty("value") && p["value"] ||
+                p.hasOwnProperty("$value") && p["$value"] ||
+                p.hasOwnProperty("val") && p["val"]
+        } else {
+            prop = property
+        }
+        try {
+            switch (asType.toUpperCase()) {
+                case "STRING":
+                    prop = prop.toString();
+                    break
+                case "NUMBER":
+                    prop = parseFloat(prop);
+                    break
+                case "INTEGER":
+                    prop = parseInt(prop);
+                    break
+                case "BOOLEAN":
+                    prop = !Boolean(["false", "no", "0", "", "undefined", "null"].indexOf(String(prop).toLowerCase().trim()));
+                    break
+                default:
+            }
+        } catch (err) {
+            if (asType) {
+                throw new Error("asType: '" + asType + "' is not possible.\n" + err.message)
+            } else {
+                // no casting requested
+            }
+        } finally {
+            return prop
+        }
+    }
+    /**
+     * Update the DOM with a template from the application.
+     * @param {HTMLElement} elem The DOM Element in which the template will be placed
+     * @param {function} tmp Function to return Template literal
+     */
     async renderElement(elem, tmp) {
         while (elem.firstChild) {
             elem.removeChild(elem.firstChild)
@@ -229,9 +229,9 @@ class Deer {
     async function expand(obj) {
         let findId = obj["@id"]
         let annos = await findByTargetId(findId)
-            // TODO: attach evidence to each property value
-            // add each value in a predictable way
-            // type properties for possible rendering?
+        // TODO: attach evidence to each property value
+        // add each value in a predictable way
+        // type properties for possible rendering?
         for (let i = 0; i < annos.length; i++) {
             let body = annos[i].body
             if (!Array.isArray(body)) {
@@ -337,7 +337,7 @@ class Deer {
      * @param {any} options specific to the template function
      */
     async getTemplate(obj, typeOverride, options) {
-        let templateFunction = function() {}
+        let templateFunction = function () {}
         let type = typeOverride || obj["@type"] || "default"
         templateFunction = this.TEMPLATES[type]
         setClass(obj["@type"])
@@ -348,9 +348,9 @@ class Deer {
     renderProp(obj, options) {
         let prop = options.prop
         let altLabel = options.altLabel || prop
-        let prefix = (options.prefix || "deer")+"-"
+        let prefix = (options.prefix || "deer") + "-"
         try {
-            let pattern = new RegExp("("+prefix+")+","g")
+            let pattern = new RegExp("(" + prefix + ")+", "g")
             return `<span class="${(prefix+prop).trim().replace(/\s+/g,"-").replace(/:/g,"-").replace(pattern,prefix).normalize("NFC").toLowerCase()}">${altLabel || prop}: ${this.getValue(obj[prop]) || "[ undefined ]"}</span>`
         } catch (err) {
             return null
@@ -367,28 +367,28 @@ class Deer {
         }
     }
 
-    renderEntity(obj, options={}) {
+    renderEntity(obj, options = {}) {
         let elem = `<label>${this.getValue(obj[options.label])||this.getValue(obj.name)||this.getValue(obj.label)||"[ unlabeled ]"}</label>`
         let tmp = []
         for (prop in obj) {
-            tmp += renderProp(obj,options)
+            tmp += renderProp(obj, options)
         }
         return elem
     }
 
-    renderPerson(obj, options={}){
+    renderPerson(obj, options = {}) {
         try {
             let elem = `<label>${this.getValue(obj.label)||this.getValue(obj.name)||this.getValue(obj.label)||"[ unlabeled ]"}</label>`
             elem += `<div class="mc-name">${this.getValue(obj[options.familyName])||this.getValue(obj.familyName)||"[ unknown ]"}, ${this.getValue(obj[options.givenName])||this.getValue(obj.givenName)||""}</div>`
-            elem += renderProp(obj,options.birthDate||"birthDate", "Birth Date") + renderProp(obj,options.deathDate||"deathDate","Death Date")
-            elem += renderDepiction(obj,options) 
+            elem += renderProp(obj, options.birthDate || "birthDate", "Birth Date") + renderProp(obj, options.deathDate || "deathDate", "Death Date")
+            elem += renderDepiction(obj, options)
             return elem
         } catch (err) {
             return null
         }
         return null
     }
-    renderEvent(obj, options){
+    renderEvent(obj, options) {
         let elem = ``
         try {
             return elem
@@ -397,7 +397,7 @@ class Deer {
         }
         return null
     }
-    renderList(obj, options){
+    renderList(obj, options) {
         let elem = ``
         try {
             return elem
@@ -408,4 +408,4 @@ class Deer {
     }
 }
 
-export default Deer
+export {Deer}
---
+++ web/entities/Annotation.js
@@ -0,0 +1,84 @@
+/**
+ * @module Data Encoding and Exhibition for RERUM (DEER)
+ * @author Patrick Cuba <cubap@slu.edu>
+ * This code should serve as a basis for developers wishing to
+ * use TinyThings as a RERUM proxy for an application for data entry,
+ * especially within the Eventities model.
+ * @see tiny.rerum.io
+ */
+class Annotation {
+    constructor(target, options){
+        this["@context"] = ""
+        this["@type"] = "Annotation"
+        this.motivation = "describing"
+        this.target = target
+        this.body = options.body || {}
+    }
+
+    setBody(body) {
+        this.body = body
+    }
+
+    getRenderTemplate() {
+        let elem = `<span>`
+        let body = this.body
+        if(!Array.isArray(body)){
+            body = [body]
+        }
+        for(let val of this.body) {
+            elem += `<span>${this.getValue(val)}</span>`
+        }
+        return elem += `</span>`
+    }
+
+    getEntryTemplate() {
+
+    }
+
+    getValue(property, asType) {
+        // TODO: There must be a best way to do this...
+        let prop;
+        if (!Array.isArray(property)) {
+            prop = property.map(getValue)
+        }
+        if (typeof property === "object") {
+            // TODO: JSON-LD insists on "@value", but this is simplified in a lot
+            // of contexts. Reading that is ideal in the future.
+            prop =
+                p.hasOwnProperty("@value") && p["@value"] ||
+                p.hasOwnProperty("value") && p["value"] ||
+                p.hasOwnProperty("$value") && p["$value"] ||
+                p.hasOwnProperty("val") && p["val"]
+        } else {
+            prop = property
+        }
+        try {
+            switch (asType.toUpperCase()) {
+                case "STRING":
+                    prop = prop.toString();
+                    break
+                case "NUMBER":
+                    prop = parseFloat(prop);
+                    break
+                case "INTEGER":
+                    prop = parseInt(prop);
+                    break
+                case "BOOLEAN":
+                    prop = !Boolean(["false", "no", "0", "", "undefined", "null"].indexOf(String(prop).toLowerCase().trim()));
+                    break
+                default:
+            }
+        } catch (err) {
+            if (asType) {
+                throw new Error("asType: '" + asType + "' is not possible.\n" + err.message)
+            } else {
+                // no casting requested
+            }
+        } finally {
+            return prop
+        }
+    }
+
+}
+
+export { Annotation }
